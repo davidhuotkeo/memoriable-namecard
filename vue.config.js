6603e19f3e99117260eb9906e5a47ab56385a755
David <devit.huotkeo@nbc.org.kh>
2020-11-29 10:43:11 +0700

    config icon

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// Inside vue.config.js
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'My App',
+      themeColor: '#942F37',
+      msTileColor: '#942F37',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+    }
+  }
