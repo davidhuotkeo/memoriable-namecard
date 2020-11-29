0245e7a14d90e41392ec213f2ae2cd426d8ad6e9
David <devit.huotkeo@nbc.org.kh>
2020-11-29 13:38:33 +0700

    update icon

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
-// Inside vue.config.js
-module.exports = {
+  module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
-      name: 'My App',
-      themeColor: '#942F37',
-      msTileColor: '#942F37',
-      appleMobileWebAppCapable: 'yes',
-      appleMobileWebAppStatusBarStyle: 'black',
+        name: 'Dave Namecard',
+        short_name: 'ave Namecard',
+        themeColor: '#942F37',
+        msTileColor: '#942F37',
+        backgroundColor: '#942F37',
+        manifestOptions: {
+            icons: [
+                {
+                    src: "img/icons/android-chrome-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "img/icons/android-chrome-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                }
+            ]
+        }
     }
-  }
+}
